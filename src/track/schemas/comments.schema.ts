7ffd7bfa80c6e5a9a9d025b96ce3ab8face81734
Yaroslav <hodosov.y@mail.ru>
2021-12-19 19:40:00 +0300

    add creacte and get  to api

--- src/track/schemas/comments.schema.ts
+++ src/track/schemas/comments.schema.ts
@@ -1,7 +1,9 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import * as mongoose from 'mongoose'
+import { Track } from './track.schema';
 
-export type CommentkDocument = Comment & Document;
+export type CommentDocument = Comment & Document;
 
 @Schema()
 export class Comment {
@@ -10,6 +12,9 @@ export class Comment {
 
     @Prop()
     text: string;
+
+    @Prop({ type: mongoose.Types.ObjectId, ref: 'Track' })
+    track: Track;
 }
 
 export const CommentkSchema = SchemaFactory.createForClass(Comment);
--- src/track/track.madule.ts
+++ src/track/track.madule.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Comment, CommentkSchema } from "./schemas/comments.schema";
+import { Track, TrackSchema } from "./schemas/track.schema";
 import { TrackController } from "./track.controller";
 import { TrackService } from "./track.service";
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Track.name, schema: TrackSchema }]),
+        MongooseModule.forFeature([{ name: Comment.name, schema: CommentkSchema }]),
+    ],
     controllers: [TrackController],
     providers: [TrackService]
 })
