1e68c68671a7f78db0e906deb5d8ddd4c4edcc8c
Yaroslav <hodosov.y@mail.ru>
2021-12-22 18:33:59 +0300

    add file logic

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from "path";
+import { FileModule } from "./file/file.module";
 import { TrackModule } from "./track/track.madule";
 
 
 @Module({
     imports: [
+        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.9i2bf.mongodb.net/next-music?retryWrites=true&w=majority'),
+        ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static'), }),
         TrackModule,
-        MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.9i2bf.mongodb.net/next-music?retryWrites=true&w=majority')]
+        FileModule,
+    ]
 })
 export class AppModule { }
--- server/src/track/track.madule.ts
+++ server/src/track/track.madule.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { FileService } from "src/file/file.service";
 import { Comment, CommentkSchema } from "./schemas/comments.schema";
 import { Track, TrackSchema } from "./schemas/track.schema";
 import { TrackController } from "./track.controller";
@@ -11,7 +12,7 @@ import { TrackService } from "./track.service";
         MongooseModule.forFeature([{ name: Comment.name, schema: CommentkSchema }]),
     ],
     controllers: [TrackController],
-    providers: [TrackService]
+    providers: [TrackService, FileService]
 })
 export class TrackModule {
 
