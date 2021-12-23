6d2181332d498bcc5abdb3f899764a69c0545d51
Yaroslav <hodosov.y@mail.ru>
2021-12-23 21:38:38 +0300

    init client

---
+++ client/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Button, } from '@chakra-ui/react'
+
+const Home: NextPage = () => {
+  return (
+    <Button colorScheme='cyan' size='md'>
+      Button
+    </Button>
+  )
+}
+
+export default Home
